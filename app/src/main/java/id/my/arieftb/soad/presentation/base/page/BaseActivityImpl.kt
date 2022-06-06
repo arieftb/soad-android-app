ee532568feca2eae99956496209b34571f435bb5
arieftb <arieftb22@gmail.com>
2022-06-06 22:19:47 +0700

    feat(presentation.Base): add push fragment method in base activity

--- app/src/main/java/id/my/arieftb/soad/presentation/base/page/BaseActivityImpl.kt
+++ app/src/main/java/id/my/arieftb/soad/presentation/base/page/BaseActivityImpl.kt
@@ -2,6 +2,7 @@ package id.my.arieftb.soad.presentation.base.page
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseActivityImpl<VB : ViewBinding> : AppCompatActivity(), BaseActivity<VB> {
@@ -18,5 +19,10 @@ abstract class BaseActivityImpl<VB : ViewBinding> : AppCompatActivity(), BaseAct
     override fun onDestroy() {
         detachBinding()
         super.onDestroy()
+    }
+
+    override fun pushFragment(fragment: Fragment, frameId: Int) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(frameId, fragment).commit()
     }
 }
--- app/src/main/java/id/my/arieftb/soad/presentation/base/page/BasePage.kt
+++ app/src/main/java/id/my/arieftb/soad/presentation/base/page/BasePage.kt
@@ -1,5 +1,6 @@
 package id.my.arieftb.soad.presentation.base.page
 
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 interface BasePage<VB: ViewBinding> {
@@ -10,4 +11,6 @@ interface BasePage<VB: ViewBinding> {
     fun detachBinding() {
         binding = null
     }
+
+    fun pushFragment(fragment: Fragment,frameId: Int)
 }
