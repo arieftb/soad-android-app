7b981e38af6536e0a49d7829ddd558cd851ca9e8
arieftb <arieftb22@gmail.com>
2022-06-06 22:12:47 +0700

    feat(presentation.Login): add navigation to main dummy

--- app/src/main/java/id/my/arieftb/soad/presentation/page/login/LoginActivity.kt
+++ app/src/main/java/id/my/arieftb/soad/presentation/page/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package id.my.arieftb.soad.presentation.page.login
 import android.os.Bundle
 import id.my.arieftb.soad.databinding.ActivityLoginBinding
 import id.my.arieftb.soad.presentation.base.page.BaseActivityImpl
+import id.my.arieftb.soad.presentation.page.main.MainActivity
 import id.my.arieftb.soad.presentation.page.register.RegisterActivity
 
 class LoginActivity : BaseActivityImpl<ActivityLoginBinding>(), LoginContract.View {
@@ -19,7 +20,7 @@ class LoginActivity : BaseActivityImpl<ActivityLoginBinding>(), LoginContract.Vi
         binding?.apply {
             buttonSignIn.setOnClickListener {
                 if (fieldEmail.isValid && fieldPassword.isValid) {
-                    // TODO: Do Login Here
+                    startActivity(MainActivity.navigate(this@LoginActivity))
                 }
             }
 
