e3904e36056bdff1d04d0e94871ec59c034440f0
arieftb <arieftb22@gmail.com>
2023-01-26 06:51:50 +0700

    implement recaptcha enterprise

--- app/src/main/java/id/my/arieftb/soad/presentation/page/login/LoginActivity.kt
+++ app/src/main/java/id/my/arieftb/soad/presentation/page/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package id.my.arieftb.soad.presentation.page.login
 
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.google.android.recaptcha.Recaptcha
+import com.google.android.recaptcha.RecaptchaAction
+import com.google.android.recaptcha.RecaptchaClient
+import id.my.arieftb.soad.BuildConfig
 import id.my.arieftb.soad.databinding.ActivityLoginBinding
 import id.my.arieftb.soad.presentation.base.page.BaseActivityImpl
-import id.my.arieftb.soad.presentation.page.main.MainActivity
 import id.my.arieftb.soad.presentation.page.register.RegisterActivity
+import kotlinx.coroutines.launch
 
 class LoginActivity : BaseActivityImpl<ActivityLoginBinding>(), LoginContract.View {
+
+    private var recaptchaClient: RecaptchaClient? = null
+
     override fun initViewBinding(): ActivityLoginBinding {
         return ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -18,10 +27,28 @@ class LoginActivity : BaseActivityImpl<ActivityLoginBinding>(), LoginContract.Vi
 
     override fun init() {
         binding?.apply {
-            buttonSignIn.setOnClickListener {
-                if (fieldEmail.isValid && fieldPassword.isValid) {
-                    startActivity(MainActivity.navigate(this@LoginActivity))
+            lifecycleScope.launch {
+                Recaptcha.getClient(application, BuildConfig.RECAPTCHA_KEY)
+                    .onSuccess {
+                        Log.d("RECAPTCHA INIT", "init: success")
+                        recaptchaClient = it
+                    }.onFailure {
+                        it.printStackTrace()
+                    }
+
+                buttonSignIn.setOnClickListener {
+                    lifecycleScope.launch {
+                        recaptchaClient?.execute(RecaptchaAction.LOGIN)?.onSuccess {
+                            Log.d("RECAPTCHA", "init Token: $it ")
+                        }?.onFailure {
+                            it.printStackTrace()
+                        }
+                    }
                 }
+//                if (fieldEmail.isValid && fieldPassword.isValid) {
+//                    startActivity(MainActivity.navigate(this@LoginActivity))
+//                }
+
             }
 
             labelCreateAccount.setOnClickListener {
