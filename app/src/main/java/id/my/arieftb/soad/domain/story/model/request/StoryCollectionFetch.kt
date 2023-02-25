f904c53d38b200df7707bf96c6de7083b0a0c101
arieftb <arieftb22@gmail.com>
2023-02-25 20:00:32 +0700

    REFACTOR(Story Collection Fetch): Change Attribute Declaration

--- app/src/main/java/id/my/arieftb/soad/domain/story/model/request/StoryCollectionFetch.kt
+++ app/src/main/java/id/my/arieftb/soad/domain/story/model/request/StoryCollectionFetch.kt
@@ -4,21 +4,21 @@ import id.my.arieftb.soad.domain.common.constant.Request
 import id.my.arieftb.soad.domain.common.model.RequestMapper
 
 class StoryCollectionFetch(
-    val page: String,
-    val size: String,
+    val page: Int,
+    val size: Int,
+    val accessToken: String? = null,
+    val location: Boolean = false,
 ) : RequestMapper.Header, RequestMapper.Query {
-    lateinit var accessToken: String
-    var useLocation: Int = 0
 
     override fun toHeader(): Map<String, Any> {
         return HashMap<String, Any>().apply {
-            this[Request.AUTHORIZATION] = accessToken
+            this[Request.AUTHORIZATION] = accessToken!!
         }
     }
 
     override fun toQuery(): Map<String, Any> {
         return HashMap<String, Any>().apply {
-            this[Request.LOCATION] = useLocation
+            this[Request.LOCATION] = location
             this[Request.PAGE] = page
             this[Request.SIZE] = size
         }
