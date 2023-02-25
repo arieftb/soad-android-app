ab92b4544cd7bd3918bc4916e073fc2a0d4bc038
arieftb <arieftb22@gmail.com>
2023-02-25 14:03:56 +0700

    FEAT(Story Domain): Add Entity

---
+++ app/src/main/java/id/my/arieftb/soad/domain/story/model/Story.kt
@@ -0,0 +1,9 @@
+package id.my.arieftb.soad.domain.story.model
+
+class Story(
+    val id: String,
+    val name: String,
+    val photo: String,
+    val description: String,
+    val date: String
+)
