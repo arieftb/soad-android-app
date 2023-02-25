4a8c12a464d9ec481ff083d2e5f68b221d07ea7a
arieftb <arieftb22@gmail.com>
2023-02-25 20:00:58 +0700

    REFACTOR(Story): Change name to be senderName

--- app/src/main/java/id/my/arieftb/soad/domain/story/model/Story.kt
+++ app/src/main/java/id/my/arieftb/soad/domain/story/model/Story.kt
@@ -2,7 +2,7 @@ package id.my.arieftb.soad.domain.story.model
 
 class Story(
     val id: String,
-    val name: String,
+    val senderName: String,
     val photo: String,
     val description: String,
     val date: String
