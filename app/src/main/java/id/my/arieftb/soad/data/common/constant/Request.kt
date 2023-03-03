0d90961bae3dcfe95c9d01ceb4421aca0b874c06
arieftb <arieftb22@gmail.com>
2023-03-04 02:27:03 +0700

    REFACTOR(Request): Move to Data

--- app/src/main/java/id/my/arieftb/soad/data/common/constant/Request.kt
+++ app/src/main/java/id/my/arieftb/soad/data/common/constant/Request.kt
@@ -1,8 +1,12 @@
-package id.my.arieftb.soad.domain.common.constant
+package id.my.arieftb.soad.data.common.constant
 
 object Request {
     const val SIZE: String = "size"
     const val PAGE: String = "page"
     const val AUTHORIZATION = "Authorization"
     const val LOCATION = "location"
+
+    const val NAME: String = "name"
+    const val EMAIL: String = "email"
+    const val PASSWORD: String = "password"
 }
