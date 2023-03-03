a2e6d1f62fde56a48ce342467c5c391c61a8d4a3
arieftb <arieftb22@gmail.com>
2023-03-04 02:36:16 +0700

    FEAT(Data Source Profile): Add Create Method

--- app/src/main/java/id/my/arieftb/soad/data/profile/source/remote/ProfileRemoteDataSourceRetrofit.kt
+++ app/src/main/java/id/my/arieftb/soad/data/profile/source/remote/ProfileRemoteDataSourceRetrofit.kt
@@ -1,8 +1,34 @@
 package id.my.arieftb.soad.data.profile.source.remote
 
+import id.my.arieftb.soad.data.common.exception.HTTPException
+import id.my.arieftb.soad.data.common.utils.RemoteErrorMessageUtils
 import id.my.arieftb.soad.data.common.utils.retrofit.service.ProfileRemoteService
+import id.my.arieftb.soad.data.profile.model.create.ProfileCreateRemoteResponse
+import id.my.arieftb.soad.data.profile.model.create.ProfileCreateRequest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ProfileRemoteDataSourceRetrofit @Inject constructor(
     private val profileRemoteService: ProfileRemoteService
-) : ProfileRemoteDataSource
+) : ProfileRemoteDataSource {
+    override fun create(param: ProfileCreateRequest): Flow<ProfileCreateRemoteResponse> {
+        return flow {
+            emit(profileRemoteService.create(param.toBody()))
+        }.flatMapConcat {
+            if (it.isSuccessful) {
+                return@flatMapConcat flow {
+                    throw HTTPException(
+                        it.code(),
+                        RemoteErrorMessageUtils.toMessage(it.errorBody()?.charStream()!!)
+                    )
+                }
+            }
+
+            flow {
+                emit(it.body()!!)
+            }
+        }
+    }
+}
